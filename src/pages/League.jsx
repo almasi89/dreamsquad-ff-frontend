bf1cdee7afffeaa4c11ea50d2411ac7f5d47aa23
Almasi <almasi89>
2025-06-24 14:45:42 +0300

    adds league jsx page

---
+++ src/pages/League.jsx
@@ -0,0 +1,13 @@
+import LeagueList from '../components/LeagueList';
+import Leaderboard from '../components/Leaderboard';
+
+function League() {
+  return (
+    <div className="min-h-screen bg-gray-900">
+      <LeagueList />
+      <Leaderboard />
+    </div>
+  );
+}
+
+export default League;     League.jsx
